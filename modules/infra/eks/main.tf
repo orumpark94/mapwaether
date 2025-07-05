770a4334b47a05ce6a4c10ce7c68bd88a40dd063
orumpark94 <nowd940@gmail.com>
2025-07-05 15:55:27 +0900

    kubectl aws-auth config map 수정 코드 추가

--- modules/infra/eks/main.tf
+++ modules/infra/eks/main.tf
@@ -2,22 +2,19 @@ provider "aws" {
   region = var.region
 }
 
-# (1) EKS 전용 최신 AMI 조회 (삭제 가능, 사용하지 않음)
-# → 삭제 또는 주석 처리 가능 (선택사항)
-
-# (2) IAM 역할 - EKS 클러스터용
+# (1) EKS 클러스터용 IAM 역할
 resource "aws_iam_role" "eks_cluster" {
   name = "${var.name}-eks-cluster-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [ {
+    Statement = [{
       Action = "sts:AssumeRole",
       Principal = {
         Service = "eks.amazonaws.com"
       },
       Effect = "Allow",
       Sid    = ""
-    } ]
+    }]
   })
 }
 
@@ -26,19 +23,19 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-# (3) IAM 역할 - EKS Node용
+# (2) EKS Node용 IAM 역할
 resource "aws_iam_role" "eks_node" {
   name = "${var.name}-eks-node-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [ {
+    Statement = [{
       Action = "sts:AssumeRole",
       Principal = {
         Service = "ec2.amazonaws.com"
       },
       Effect = "Allow",
       Sid    = ""
-    } ]
+    }]
   })
 }
 
@@ -57,9 +54,7 @@ resource "aws_iam_role_policy_attachment" "ec2_container_registry_read_only" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
-# (4) Launch Template 제거됨 ✅
-
-# (5) EKS 클러스터 생성
+# (3) EKS 클러스터 생성
 resource "aws_eks_cluster" "this" {
   name     = "${var.name}-eks-cluster"
   role_arn = aws_iam_role.eks_cluster.arn
@@ -74,7 +69,7 @@ resource "aws_eks_cluster" "this" {
   depends_on = [aws_iam_role.eks_cluster]
 }
 
-# (6) EKS Node Group 생성 (✅ 간단 버전)
+# (4) EKS Node Group 생성
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${var.name}-node-group"
@@ -87,12 +82,12 @@ resource "aws_eks_node_group" "this" {
     min_size     = var.node_min_size
   }
 
-  instance_types = [var.node_instance_type] # ← 변경된 부분
+  instance_types = [var.node_instance_type]
 
   depends_on = [aws_iam_role.eks_node]
 }
 
-# (7) 클러스터 이름 SSM에 저장
+# (5) 클러스터 이름 SSM에 저장
 resource "aws_ssm_parameter" "eks_cluster_name" {
   name  = "/mapweather/eks-cluster-name"
   type  = "String"
@@ -100,50 +95,17 @@ resource "aws_ssm_parameter" "eks_cluster_name" {
   overwrite = true
 }
 
-# (8) aws-auth ConfigMap 적용
-data "aws_eks_cluster" "this" {
-  name = aws_eks_cluster.this.name
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = aws_eks_cluster.this.name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
-
-resource "kubernetes_config_map" "aws_auth" {
-  metadata {
-    name      = "aws-auth"
-    namespace = "kube-system"
-  }
-
-  data = {
-    mapRoles = yamlencode([
-      {
-        rolearn  = aws_iam_role.eks_node.arn
-        username = "system:node:{{EC2PrivateDNSName}}"
-        groups   = [
-          "system:bootstrappers",
-          "system:nodes"
-        ]
-      }
-    ])
-
-    mapUsers = yamlencode([
-      {
-        userarn  = "arn:aws:iam::863676520919:user/mapweather"
-        username = "mapweather"
-        groups   = ["system:masters"]
-      }
-    ])
-  }
-
+# (6) aws-auth ConfigMap 적용 (null_resource + local-exec)
+resource "null_resource" "apply_aws_auth" {
   depends_on = [
     aws_eks_cluster.this,
     aws_eks_node_group.this
   ]
+
+  provisioner "local-exec" {
+    command = <<EOT
+    aws eks update-kubeconfig --name ${aws_eks_cluster.this.name} --region ${var.region}
+    kubectl apply -f ./modules/infra/eks/aws-auth.yaml
+    EOT
+  }
 }
