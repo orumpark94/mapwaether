1e2cef1a48a3152e06f147baad87860b5a1017a3
orumpark94 <nowd940@gmail.com>
2025-07-06 15:12:27 +0900

    test

--- modules/infra/security/main.tf
+++ modules/infra/security/main.tf
@@ -88,7 +88,7 @@ resource "aws_security_group" "eks" {
     from_port   = 10250
     to_port     = 10250
     protocol    = "tcp"
-    cidr_blocks = ["13.124.0.0/16", "3.35.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -96,7 +96,7 @@ resource "aws_security_group" "eks" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["13.124.0.0/16", "3.35.0.0/16","210.92.246.130/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
