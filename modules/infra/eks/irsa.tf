30d20108225585948b0c905efcb079373bbc52b5
orumpark94 <nowd940@gmail.com>
2025-07-08 13:22:44 +0900

    test

--- modules/infra/eks/irsa.tf
+++ modules/infra/eks/irsa.tf
@@ -12,25 +12,29 @@ resource "aws_iam_openid_connect_provider" "this" {
 resource "null_resource" "update_irsa_trust_policy" {
   provisioner "local-exec" {
     command = <<EOT
-aws iam update-assume-role-policy \
-  --role-name mapweather-map-api-irsa-role \
-  --policy-document '{
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Federated": "${aws_iam_openid_connect_provider.this.arn}"
-        },
-        "Action": "sts:AssumeRoleWithWebIdentity",
-        "Condition": {
-          "StringEquals": {
-            "${replace(data.aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")}:sub": "system:serviceaccount:default:map-api-sa"
-          }
+cat <<EOF > assume-policy.json
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "${aws_iam_openid_connect_provider.this.arn}"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "${replace(data.aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")}:sub": "system:serviceaccount:default:map-api-sa"
         }
       }
-    ]
-  }'
+    }
+  ]
+}
+EOF
+
+aws iam update-assume-role-policy \
+  --role-name mapweather-map-api-irsa-role \
+  --policy-document file://assume-policy.json
 EOT
   }
 
