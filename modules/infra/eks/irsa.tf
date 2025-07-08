d71cd4711d2e016ed6ed63d62e0b16fac42e6cbe
orumpark94 <nowd940@gmail.com>
2025-07-08 12:00:20 +0900

    oidc 공급자 동적 생성 및 oidc 신뢰관계 업데이트

--- modules/infra/eks/irsa.tf
+++ modules/infra/eks/irsa.tf
@@ -1,7 +1,14 @@
-data "aws_iam_openid_connect_provider" "this" {
+
+# (2) IAM OIDC 공급자 생성 (존재하지 않으면 새로 생성됨)
+resource "aws_iam_openid_connect_provider" "this" {
   url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0ecd5c0f6"]
 }
 
+# (3) 기존 Role(mapweather-map-api-irsa-role)의 신뢰 정책을 OIDC 방식으로 업데이트
 resource "null_resource" "update_irsa_trust_policy" {
   provisioner "local-exec" {
     command = <<EOT
@@ -13,7 +20,7 @@ aws iam update-assume-role-policy \
       {
         "Effect": "Allow",
         "Principal": {
-          "Federated": "${data.aws_iam_openid_connect_provider.this.arn}"
+          "Federated": "${aws_iam_openid_connect_provider.this.arn}"
         },
         "Action": "sts:AssumeRoleWithWebIdentity",
         "Condition": {
@@ -28,9 +35,8 @@ EOT
   }
 
   triggers = {
-    oidc = data.aws_iam_openid_connect_provider.this.arn
+    oidc = aws_iam_openid_connect_provider.this.arn
   }
 
-  depends_on = [data.aws_eks_cluster.this]
+  depends_on = [aws_iam_openid_connect_provider.this]
 }
-
